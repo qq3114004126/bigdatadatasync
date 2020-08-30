e1438fabced43bc0cc634015eebe517385867925
qq3114004126 <1058609177@qq.com>
2020-08-30 10:53:46 +0800

    大数据数据集成demo

---
+++ src/main/java/com/example/hivehbasees/bigdatadatasyncdemo/BigdatadatasyncdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.hivehbasees.bigdatadatasyncdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigdatadatasyncdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigdatadatasyncdemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hivehbasees/bigdatadatasyncdemo/BigdatadatasyncdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hivehbasees.bigdatadatasyncdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigdatadatasyncdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
